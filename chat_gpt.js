8d19ec03242146736aa17a80af2bd9f297a1852f
cuteboy143 <71389501+cuteboy143@users.noreply.github.com>
2023-03-06 22:21:44 +0530

    Update chat_gpt.js

--- chat_gpt.js
+++ chat_gpt.js
@@ -15,8 +15,30 @@ bot.onText(/\/modify (.+)/, async function (msg, match) {
   const chatId = msg.chat.id;
 
   // Extract the last page URL from the short URL
-  const response = await axios.get(shortUrl, { maxRedirects: 0, validateStatus: false });
-  const lastUrl = response.headers.location;
+
+  const newTagId = 'HARDIK';
+
+// Fetch the final URL
+request({ url: shortUrl, followRedirect: true }, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    // Parse the final URL and extract the query string parameters
+    const urlParts = url.parse(response.request.uri.href, true);
+    const queryParams = urlParts.query;
+
+    // Modify the "tag-id" parameter with the new tag ID
+    queryParams['tag'] = newTagId;
+
+    // Remove all parameters after the "tag-id" parameter
+    let paramIndex = Object.keys(queryParams).indexOf('tag-id');
+    if (paramIndex >= 0) {
+      queryParams = Object.fromEntries(Object.entries(queryParams).slice(0, paramIndex + 1));
+    }
+
+    // Rebuild the query string and update the final URL
+    const newQuery = querystring.stringify(queryParams);
+    const lastUrl = urlParts.protocol + '//' + urlParts.host + urlParts.pathname + '?' + newQuery;
+    
+ 
 
   // Parse the last page URL and update the "tag" parameter with a new value provided by the user
   const parsedUrl = new URL(lastUrl);
